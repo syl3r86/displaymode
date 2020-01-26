22756c7b435f552fe345c9dc0ef54916c2975229
Felix <syl3r31@gmail.com>
2020-01-26 12:39:41 +0100

    Delete displaymode.js

--- displaymode/displaymode.js
+++
@@ -1,8 +0,0 @@
-Hooks.on('ready', () => {
-    $('#logo').click(ev => {
-        $('#sidebar').toggle();
-        $('#navigation').toggle();
-        $('#controls').toggle();
-        $('#players').toggle();
-    });
-});
