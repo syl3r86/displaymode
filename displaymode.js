06605bf30fe5dda484f43dc5a70e792b5f64e919
Felix <syl3r31@gmail.com>
2020-02-09 11:16:11 +0100

    Add files via upload

--- displaymode.js
+++ displaymode.js
@@ -1,8 +1,18 @@
 Hooks.on('ready', () => {
     $('#logo').click(ev => {
-        $('#sidebar').toggle();
-        $('#navigation').toggle();
-        $('#controls').toggle();
-        $('#players').toggle();
+        window.hideUI = !window.hideUI;
+        if (window.hideUI) {
+            $('#sidebar').hide();
+            $('#navigation').hide();
+            $('#controls').hide();
+            $('#players').hide();
+            $('#hotbar').hide();
+        } else {
+            $('#sidebar').show();
+            $('#navigation').show();
+            $('#controls').show();
+            $('#players').show();
+            $('#hotbar').show();
+        }
     });
 });
