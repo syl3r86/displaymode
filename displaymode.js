0a2dcaa254c8a57792270f1cf37d14f64fde3b0d
Gab <gcharbonneau@cstj.qc.ca>
2022-09-29 12:54:00 -0400

    Added compatibility for Simple Calendar, Party Ressources and Mnk's Token Bar

--- displaymode.js
+++ displaymode.js
@@ -7,17 +7,23 @@ Hooks.on('ready', () => {
             $('#controls').hide();
             $('#players').hide();
             $('#hotbar').hide();
+            $('#fvtt-party-resources-status-bar').hide();
+            $('#fsc-eg').hide();
+            $('#tokenbar-move-handle').hide();
         } else {
             $('#sidebar').show();
             $('#navigation').show();
             $('#controls').show();
             $('#players').show();
             $('#hotbar').show();
+            $('#fvtt-party-resources-status-bar').show();
+            $('#fsc-eg').show();
+            $('#tokenbar-move-handle').show();
         }
     });
 });
 
-Hooks.on('renderSceneNavigation', (app,html) => {
+Hooks.on('renderSceneNavigation', (app, html) => {
     if (window.hideUI) {
         html.hide();
     }
@@ -36,6 +42,24 @@ Hooks.on('renderSidebarTab', (app, html) => {
 });
 
 Hooks.on('renderCombatTracker', (app, html) => {
+    if (window.hideUI) {
+        html.hide();
+    }
+});
+
+Hooks.on('fvtt-party-resources-status-bar', (app, html) => {
+    if (window.hideUI) {
+        html.hide();
+    }
+});
+
+Hooks.on('fsc-eg', (app, html) => {
+    if (window.hideUI) {
+        html.hide();
+    }
+});
+
+Hooks.on('tokenbar', (app, html) => {
     if (window.hideUI) {
         html.hide();
     }
